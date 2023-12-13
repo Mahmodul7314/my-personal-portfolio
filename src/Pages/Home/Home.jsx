bc49b2a81eccd18b078296859767b350cea27435
Mahmodul7314 <mahmudulhasan7314@gmail.com>
2023-12-13 16:39:36 +0600

    portfolio website design image issue solve

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -7,7 +7,7 @@ const Home = () => {
         <div className="bg-green-50">
             <div className="lg:h-[80vh] md:h-[160vh] h-[180vh] px-10 w-3/2 lg:flex-row flex flex-col lg:justify-around">
                 <div className="lg:py-24 py-10">
-                <h2 className="text-5xl font-bold flex items-center">Hi! I am Mahmodul <span className="pl-6"><img className="w-12 h-12" src="../../../public/wave.png" alt="" /></span></h2>
+                <h2 className="text-5xl font-bold flex items-center">Hi! I am Mahmodul <span className="pl-6"><img className="w-12 h-12" src="https://i.ibb.co/j32jqvK/wave.png" alt="" /></span></h2>
                      <p className="text-lg font-medium text-gray-700 py-8">Passionate Front-End Developer with expertise in React and Node.js. <br /> Dedicated to crafting seamless digital experiences and embracing innovation.</p>
                     <p className="flex text-md text-gray-600 gap-1 font-medium items-center justify-start"><span><MdLocationPin /></span>Dhaka,Bangladesh</p>
                     <p className="flex items-center text-md gap-1 font-medium text-gray-600"><span><GoGoal /></span>actively seeking job opportunities </p>
@@ -18,7 +18,7 @@ const Home = () => {
                      </div>
                 </div>
                 <div className="lg:px-0 px-14">
-                 <img className="lg:w-[28rem] rounded-full bg-green-300 p-2 lg:h-[30rem]" src="../../../public/profile.png" alt="" />
+                 <img className="lg:w-[28rem] rounded-full bg-green-300 p-2 lg:h-[30rem]" src="https://i.ibb.co/Zf1gPTK/profile.png" alt="" />
                 </div>
             </div>
         </div>
--- src/Pages/Portfolio/Portfolio.jsx
+++ src/Pages/Portfolio/Portfolio.jsx
@@ -11,58 +11,65 @@ const Portfolio = () => {
            <div className="lg:pl-[28rem] py-14">
            <div className="carousel w-[24rem]">
   <div id="slide1" className="carousel-item relative w-full">
-    <img src="../../../public/learnme1.jpg" className="h-[60]" />
+    <img src="https://i.ibb.co/VTnZBWg/learnme1.jpg" className="h-[60]" />
     <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-      <a href="#slide4" className="btn btn-circle btn-info">❮</a> 
+      <a href="#slide8" className="btn btn-circle btn-info">❮</a> 
       <a href="#slide2" className="btn btn-circle btn-info">❯</a>
     </div>
   </div> 
   <div id="slide2" className="carousel-item relative w-full">
-    <img src="../../../public/learnme2.jpg" className="" />
+    <img src="https://i.ibb.co/Bwx4Z7t/learnme2.jpg" className="" />
     <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
       <a href="#slide1" className="btn btn-circle btn-info">❮</a> 
       <a href="#slide3" className="btn btn-circle btn-info">❯</a>
     </div>
   </div> 
   <div id="slide3" className="carousel-item relative w-full">
-    <img src="../../../public/learnme3.jpg" className="" />
+    <img src="https://i.ibb.co/tqqM0qM/learnme3.jpg" className="" />
     <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
       <a href="#slide2" className="btn btn-circle btn-info">❮</a> 
       <a href="#slide4" className="btn btn-circle btn-info">❯</a>
     </div>
   </div> 
   <div id="slide4" className="carousel-item relative w-full">
-    <img src="../../../public/learnme4.jpg" className="" />
+    <img src="https://i.ibb.co/F0R3t28/learnme4.jpg" className="" />
     <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
       <a href="#slide3" className="btn btn-circle btn-info">❮</a> 
       <a href="#slide5" className="btn btn-circle btn-info">❯</a>
     </div>
   </div>
   <div id="slide5" className="carousel-item relative w-full">
-    <img src="../../../public/az1.jpg" className="" />
+    <img src="https://i.ibb.co/hXZL2z2/learnme5.jpg" className="" />
     <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
       <a href="#slide4" className="btn btn-circle btn-info">❮</a> 
       <a href="#slide6" className="btn btn-circle btn-info">❯</a>
     </div>
   </div>
   <div id="slide6" className="carousel-item relative w-full">
-    <img src="../../../public/az2.jpg" className="w-full" />
+    <img src="https://i.ibb.co/JyDktCq/az1.jpg" className="w-full" />
     <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
       <a href="#slide5" className="btn btn-circle btn-info">❮</a> 
       <a href="#slide7" className="btn btn-circle btn-info">❯</a>
     </div>
   </div>
   <div id="slide7" className="carousel-item relative w-full">
-    <img src="../../../public/az3.jpg" className="w-full" />
+    <img src="https://i.ibb.co/TcPbcYg/az2.jpg" className="w-full" />
     <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
       <a href="#slide6" className="btn btn-circle btn-info">❮</a> 
       <a href="#slide8" className="btn btn-circle btn-info">❯</a>
     </div>
   </div>
   <div id="slide8" className="carousel-item relative w-full">
-    <img src="../../../public/az4.jpg" className="w-full" />
+    <img src="https://i.ibb.co/mGt4r8Q/az3.jpg" className="w-full" />
     <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
       <a href="#slide7" className="btn btn-circle btn-info">❮</a> 
+      <a href="#slide9" className="btn btn-circle btn-info">❯</a>
+    </div>
+  </div>
+  <div id="slide9" className="carousel-item relative w-full">
+    <img src="https://i.ibb.co/2chDbrS/az4.jpg" className="w-full" />
+    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+      <a href="#slide8" className="btn btn-circle btn-info">❮</a> 
       <a href="#slide1" className="btn btn-circle btn-info">❯</a>
     </div>
   </div>
